6cc3b01863e51557b2d6231a5b451db4bac56999
Giovanni Ricca <giovanniricca@proton.me>
2024-08-20 12:56:32 +0200

    gemstone: rro_overlays: Import power profile
    * From sunstone V816.0.5.0.UMQMIXM & moonstone V816.0.5.0.UMPINXM
    
    Change-Id: I8d7d2980c606465b060413f6b542a9c4bfa43e83

--- device.mk
+++ device.mk
@@ -7,6 +7,15 @@
 # Inherit from sm6375-common
 $(call inherit-product, device/xiaomi/sm6375-common/common.mk)
 
+# Overlay
+PRODUCT_PACKAGES += \
+    FrameworkOverlayGemstone \
+    FrameworkOverlayMoonstoneGl \
+    FrameworkOverlayMoonstoneIn
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/rro_overlays/config-odm.xml:$(TARGET_COPY_OUT_ODM)/overlay/config/config.xml
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
