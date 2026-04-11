6b452f62804c9e889ece5469ee8d440686bfc639
EmanuelCN <emanuelghub@gmail.com>
2026-04-11 00:13:47 +0000

    gemstone: Use the new location of dex2oat boot image profile

--- device.mk
+++ device.mk
@@ -32,7 +32,7 @@ PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
 
 # Use a profile-based boot image.
 PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
-PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
+PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/boot/boot-image-profile.txt
 
 # Preopt frequently used system applications.
 PRODUCT_DEXPREOPT_SPEED_APPS += \
