07092d54723fa7fb5bb4e57d1a414c94e1d4ae63
Mihir3217 <mihirgaurav394@gmail.com>
2026-08-11 11:27:19 +0000

    integrate both tree as one

--- extract-files.py
+++ extract-files.py
@@ -57,7 +57,5 @@ module = ExtractUtilsModule(
 )
 
 if __name__ == '__main__':
-    utils = ExtractUtils.device_with_common(
-        module, 'sm6375-common', module.vendor
-    )
+    utils = ExtractUtils.device(module)
     utils.run()
